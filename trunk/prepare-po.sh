ac5496ea44034533a3307388ca5e5d728a2900ab
kitone <kitone@bc142938-0c22-0410-9269-ec09724cb74d>
2008-08-13 13:32:45 +0000

    Add intltool. *.desktop file can be translated. Patch by Luboš Staněk.

--- trunk/prepare-po.sh
+++ trunk/prepare-po.sh
@@ -2,4 +2,5 @@
 find src -name "*.h" | sed -e "s/\.\///g" > po/POTFILES.in
 find src -name "*.cc" | sed -e "s/\.\///g" >> po/POTFILES.in
 find share/glade -name "*.glade" | sed -e "s/\.\///g" >> po/POTFILES.in
+find share -name "*.desktop.in" | sed -e "s/\.\///g" >> po/POTFILES.in
 cat po/POTFILES.in | sort > po/POTFILES.in
