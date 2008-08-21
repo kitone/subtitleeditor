7e2390fea8728bf792a7d4ea08ee67040e67b0af
kitone <kitone@bc142938-0c22-0410-9269-ec09724cb74d>
2008-08-21 16:24:35 +0000

    Add files ComboBoxEncoding. Remove code from DialogFileChooser.cc

--- trunk/src/gui/DialogFileChooser.cc
+++ trunk/src/gui/DialogFileChooser.cc
@@ -24,13 +24,13 @@
 #include "utility.h"
 #include "RegEx.h"
 #include "DialogCharacterCodings.h"
-
+#include "ComboBoxEncoding.h"
 
 /*
  * Used to define the separator.
  * label = "<separator>"
  */
-static bool on_row_separator_func(const Glib::RefPtr<Gtk::TreeModel> &model, const Gtk::TreeModel::iterator &it, const Gtk::TreeModelColumn<Glib::ustring> &col)
+bool on_row_separator_func(const Glib::RefPtr<Gtk::TreeModel> &model, const Gtk::TreeModel::iterator &it, const Gtk::TreeModelColumn<Glib::ustring> &col)
 {
 	Glib::ustring text = (*it)[col];
 	if(text == "<separator>")
@@ -98,177 +98,6 @@ public:
 	{
 		return get_active_text();
 	}
-};
-
-/*
- * ComboBox for choosing the encoding.
- * Only the encoding preferences are displayed.
- * The support of "Auto Detected" is enable by default.
- */
-class ComboBoxEncoding : public Gtk::ComboBoxText
-{
-public:
-
-	/*
-	 * Constructor
-	 */
-	ComboBoxEncoding(BaseObjectType* cobject, const Glib::RefPtr<Gnome::Glade::Xml>& refGlade)
-	:Gtk::ComboBoxText(cobject), m_with_auto_detected(true)
-	{
-		init_encodings();
-
-		// separator function
-		Gtk::ComboBoxText::set_row_separator_func(
-			sigc::bind(sigc::ptr_fun(on_row_separator_func), m_text_columns.m_column));
-
-		// m_connection_changed is need to disable the signal when the combobox is rebuild.
-		m_connection_changed = signal_changed().connect(
-				sigc::mem_fun(*this, &ComboBoxEncoding::on_combo_changed));
-	}
-
-	/*
-	 * Returns only the charset value.
-	 * ex: "UTF-8", "ISO-8859-15" ...
-	 * Return empty charset if it's "Auto Detected".
-	 */
-	Glib::ustring get_value() const
-	{
-		if(m_with_auto_detected)
-		{
-			if(get_active_row_number() == 0)
-				return Glib::ustring(); // "None"
-		}
-
-		// extract the charset 
-		// ex:
-		// "Unicode (UTF-8)"
-		Glib::ustring text = get_active_text();
-
-		Glib::ustring::size_type a = text.find('(');
-		Glib::ustring::size_type b = text.find(')', a);
-
-		if(a != Glib::ustring::npos && b != Glib::ustring::npos)
-			return text.substr(a+1, b-a-1);
-
-		return Glib::ustring();
-	}
-
-	/*
-	 * Enable or disable the auto detected mode.
-	 */
-	void show_auto_detected(bool value)
-	{
-		m_with_auto_detected = value;
-
-		bool state = is_sensitive();
-		set_sensitive(false);
-
-		init_encodings();
-
-		set_sensitive(state);
-	}
-
-protected:
-
-	/*
-	 * Rebuild the combobox with encoding user preferences.
-	 */
-	void init_encodings()
-	{
-		m_connection_changed.block();
-
-		clear();
-
-		bool used_auto_detected = Config::getInstance().get_value_bool("encodings", "used-auto-detected");
-
-		if(m_with_auto_detected)
-		{
-			append_text(_("Auto Detected"));
-			append_text("<separator>");
-		}
-
-		std::list<Glib::ustring> encodings = 
-			Config::getInstance().get_value_string_list("encodings", "encodings");
-
-		if(encodings.empty())
-		{
-			std::string charset;
-			Glib::get_charset(charset);
-
-			Glib::ustring item;
-			item += _("Current Locale");
-			item += " (";
-			item += charset;
-			item += ")";
-
-			append_text(item);
-		}
-		else
-		{
-			std::list<Glib::ustring>::const_iterator it;
-			for(it = encodings.begin(); it != encodings.end(); ++it)
-			{
-				EncodingInfo *info = Encodings::get_from_charset(*it);
-				if(info)
-				{
-					Glib::ustring item;
-					item+= info->name;
-					item+= " (";
-					item+= info->charset;
-					item+= ")";
-
-					append_text(item);
-				}
-			}
-		}
-		// configure
-		append_text("<separator>");
-		append_text(_("Add or Remove..."));
-
-		if(m_with_auto_detected)
-		{
-			if(used_auto_detected)
-				set_active(0);
-			else
-				set_active(2); // auto detected (0), separator (1), first charset (2)
-		}
-		else
-			set_active(0);
-
-		m_connection_changed.unblock();
-	}
-
-	/*
-	 * Gtk::ComboBox::on_changed
-	 * Used for intercepte "Add or Remove..."
-	 */
-	void on_combo_changed()
-	{
-		unsigned int size = get_model()->children().size();
-		unsigned int activated = get_active_row_number();
-
-		if(activated == size-1)
-		{
-			std::auto_ptr<DialogCharacterCodings> dialog = DialogCharacterCodings::create();
-			if(dialog->run() == Gtk::RESPONSE_OK)
-			{
-				init_encodings();
-			}
-			else if(m_with_auto_detected)
-			{
-				if(Config::getInstance().get_value_bool("encodings", "used-auto-detected"))
-					set_active(0);
-				else
-					set_active(2);
-			}
-			else
-				set_active(0);
-		}
-	}
-
-protected:
-	bool m_with_auto_detected;
-	sigc::connection m_connection_changed;
 };
 
 /*
