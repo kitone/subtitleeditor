cd342103b4b636f82366c19dd3d635f2ac17638a
kitone <kitone@users.noreply.github.com>
2008-09-25 11:07:18 +0000

    Add option [encoding: UTF-8].

--- prepare-po.sh
+++ prepare-po.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
-find src -name "*.h" | sed -e "s/\.\///g" > po/POTFILES.in
+echo '[encoding: UTF-8]' > po/POTFILES.in
+find src -name "*.h" | sed -e "s/\.\///g" >> po/POTFILES.in
 find src -name "*.cc" | sed -e "s/\.\///g" >> po/POTFILES.in
 find share/glade -name "*.glade" | sed -e "s/\.\///g" >> po/POTFILES.in
 find share -name "*.desktop.in" | sed -e "s/\.\///g" >> po/POTFILES.in
