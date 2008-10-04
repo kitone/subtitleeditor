cb02f441dd383deb7c46973fdb6f5baa5a37e529
kitone <kitone@bc142938-0c22-0410-9269-ec09724cb74d>
2008-10-04 13:16:20 +0000

    Help to Read or write file.

---
+++ trunk/src/FileWriter.cc
@@ -0,0 +1,55 @@
+/*
+ *	subtitleeditor -- a tool to create or edit subtitle
+ *
+ *	http://home.gna.org/subtitleeditor/
+ *	https://gna.org/projects/subtitleeditor/
+ *
+ *	Copyright @ 2005-2008, kitone
+ *
+ *	This program is free software; you can redistribute it and/or modify
+ *	it under the terms of the GNU General Public License as published by
+ *	the Free Software Foundation; either version 3 of the License, or
+ *	(at your option) any later version.
+ *
+ *	This program is distributed in the hope that it will be useful,
+ *	but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *	GNU General Public License for more details.
+ *
+ *	You should have received a copy of the GNU General Public License
+ *	along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include "FileWriter.h"
+#include <fstream>
+#include "Error.h"
+#include "utility.h"
+#include "Encodings.h"
+
+/*
+ *
+ */
+FileWriter::FileWriter(const Glib::ustring &uri, const Glib::ustring &charset, const Glib::ustring &newline)
+{
+	m_uri = uri;
+	m_charset = charset;
+	m_newline = newline;
+}
+
+/*
+ * Write to the file.
+ */
+void FileWriter::to_file()
+{
+	std::string data = Encoding::convert_from_utf8_to_charset(str(), m_charset); 
+	
+	// FIXME: newline
+
+	std::ofstream file(Glib::filename_from_uri(m_uri).c_str());
+	if(!file)
+		throw IOFileError(_("Couldn't open the file."));
+
+	file << data;
+	file.close();
+}
+
