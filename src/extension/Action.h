34ba7c26a3b3b1d417a3572db6bf84cc294a48cf
kitone <kitone@users.noreply.github.com>
2008-11-03 11:19:33 +0000

    New file, Action extension.

---
+++ src/extension/Action.h
@@ -0,0 +1,93 @@
+#ifndef _Action_h
+#define _Action_h
+
+/*
+ *	subtitleeditor -- a tool to create or edit subtitle
+ *
+ *	http://home.gna.org/subtitleeditor/
+ *	https://gna.org/projects/subtitleeditor/
+ *
+ *	Copyright @ 2005-2008, kitone
+ *
+ *	This program is free software; you can redistribute it and/or modify
+ *	it under the terms of the GNU General Public License as published by
+ *	the Free Software Foundation; either version 3 of the License, or
+ *	(at your option) any later version.
+ *
+ *	This program is distributed in the hope that it will be useful,
+ *	but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *	GNU General Public License for more details.
+ *
+ *	You should have received a copy of the GNU General Public License
+ *	along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include <gtkmm.h>
+#include "SubtitleEditorWindow.h"
+#include "Config.h"
+#include "Extension.h"
+
+
+class Action : public Extension, public sigc::trackable
+{
+public:
+
+	/*
+	 *
+	 */
+	Action();
+
+	/*
+	 *
+	 */
+	virtual ~Action();
+
+	/*
+	 *
+	 */
+	virtual void activate();
+
+	/*
+	 *
+	 */
+	virtual void deactivate();
+
+	/*
+	 *
+	 */
+	virtual void update_ui();
+
+	/*
+	 *	static method
+	 */
+
+	/*
+	 *
+	 */
+	static SubtitleEditorWindow* get_subtitleeditor_window();
+
+	/*
+	 *
+	 */
+	static Config& get_config();
+
+	/*
+	 *
+	 */
+	static Document* get_current_document();
+
+	/*
+	 *
+	 */
+	static Glib::RefPtr<Gtk::UIManager> get_ui_manager();
+
+};
+
+#define REGISTER_EXTENSION(classname) \
+	extern "C" Extension* extension_register() \
+	{ \
+		return new classname; \
+	}
+
+#endif//_Action_h
