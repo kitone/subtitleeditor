22be3de44dd8e7aa19b503767c5af631aac69edd
kitone <kitone@users.noreply.github.com>
2008-11-06 09:49:01 +0000

    Add plugins directory.

--- prepare-po.sh
+++ prepare-po.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 echo '[encoding: UTF-8]' > po/POTFILES.in
-find src -name "*.h" | sed -e "s/\.\///g" >> po/POTFILES.in
-find src -name "*.cc" | sed -e "s/\.\///g" >> po/POTFILES.in
 find share/glade -name "*.glade" | sed -e "s/\.\///g" >> po/POTFILES.in
 find share -name "*.desktop.in" | sed -e "s/\.\///g" >> po/POTFILES.in
+find src -type f | grep '\.\(h\|cc\)$' | sed -e "s/\.\///g" >> po/POTFILES.in
+find plugins -type f | grep '\.\(h\|cc\|glade\)$' | sed -e "s/\.\///g" >> po/POTFILES.in
 cat po/POTFILES.in | sort > po/POTFILES.in
