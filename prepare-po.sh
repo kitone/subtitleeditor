4643a73feab98fa01f75f49b934c5224eb2ff9e2
kitone <kitone@users.noreply.github.com>
2008-11-12 10:39:50 +0000

    Add desktop.in files.

--- prepare-po.sh
+++ prepare-po.sh
@@ -3,5 +3,5 @@ echo '[encoding: UTF-8]' > po/POTFILES.in
 find share/glade -name "*.glade" | sed -e "s/\.\///g" >> po/POTFILES.in
 find share -name "*.desktop.in" | sed -e "s/\.\///g" >> po/POTFILES.in
 find src -type f | grep '\.\(h\|cc\)$' | sed -e "s/\.\///g" >> po/POTFILES.in
-find plugins -type f | grep '\.\(h\|cc\|glade\)$' | sed -e "s/\.\///g" >> po/POTFILES.in
+find plugins -type f | grep '\.\(h\|cc\|glade\|desktop\.in\)$' | sed -e "s/\.\///g" >> po/POTFILES.in
 cat po/POTFILES.in | sort > po/POTFILES.in
