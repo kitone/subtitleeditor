9b5101d3af4ea61a7c098fd41fc13f04cbd25b6a
kitone <kitone@users.noreply.github.com>
2008-11-13 17:58:14 +0000

    Add [type: gettext/ini] before the se-plugin name.

--- prepare-po.sh
+++ prepare-po.sh
@@ -3,5 +3,6 @@ echo '[encoding: UTF-8]' > po/POTFILES.in
 find share/glade -name "*.glade" | sed -e "s/\.\///g" >> po/POTFILES.in
 find share -name "*.desktop.in" | sed -e "s/\.\///g" >> po/POTFILES.in
 find src -type f | grep '\.\(h\|cc\)$' | sed -e "s/\.\///g" >> po/POTFILES.in
-find plugins -type f | grep '\.\(h\|cc\|glade\|se-plugin\.in\)$' | sed -e "s/\.\///g" >> po/POTFILES.in
+find plugins -type f | grep '\.\(h\|cc\|glade\)$' | sed -e "s/\.\///g" >> po/POTFILES.in
+find plugins -type f | grep '\.se-plugin\.in$' | sed -e "s/\(.*\)/\[type: gettext\/ini\] \1/" >> po/POTFILES.in
 cat po/POTFILES.in | sort > po/POTFILES.in
