ef16f57477d231e1b62d279f4cd5365c1a6dfef9
kitone <kitone@users.noreply.github.com>
2009-01-13 13:40:18 +0000

    Introduce se-pattern support.

--- prepare-po.sh
+++ prepare-po.sh
@@ -5,4 +5,5 @@ find share -name "*.desktop.in" | sed -e "s/\.\///g" >> po/POTFILES.in
 find src -type f | grep '\.\(h\|cc\)$' | sed -e "s/\.\///g" >> po/POTFILES.in
 find plugins -type f | grep '\.\(h\|cc\|glade\)$' | sed -e "s/\.\///g" >> po/POTFILES.in
 find plugins -type f | grep '\.se-plugin\.in$' | sed -e "s/\(.*\)/\[type: gettext\/ini\] \1/" >> po/POTFILES.in
+find plugins -type f | grep '\.se-pattern\.in$' | sed -e "s/\(.*\)/\[type: gettext\/xml\] \1/" >> po/POTFILES.in
 cat po/POTFILES.in | sort > po/POTFILES.in
