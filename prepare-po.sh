f9df5c3dce306cfea1abda4653ef9dc1398f1bf1
kitone <kitone@users.noreply.github.com>
2009-08-19 14:02:55 +0000

    Change glade to ui.

--- prepare-po.sh
+++ prepare-po.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 echo '[encoding: UTF-8]' > po/POTFILES.in
-find share/glade -name "*.glade" | sed -e "s/\.\///g" >> po/POTFILES.in
+find share/ui -name "*.ui" | sed -e "s/\.\///g" >> po/POTFILES.in
 find share -name "*.desktop.in" | sed -e "s/\.\///g" >> po/POTFILES.in
 find src -type f | grep '\.\(h\|cc\)$' | sed -e "s/\.\///g" >> po/POTFILES.in
-find plugins -type f | grep '\.\(h\|cc\|glade\)$' | sed -e "s/\.\///g" >> po/POTFILES.in
+find plugins -type f | grep '\.\(h\|cc\|ui\)$' | sed -e "s/\.\///g" >> po/POTFILES.in
 find plugins -type f | grep '\.se-plugin\.in$' | sed -e "s/\(.*\)/\[type: gettext\/ini\] \1/" >> po/POTFILES.in
 find plugins -type f | grep '\.se-pattern\.in$' | sed -e "s/\(.*\)/\[type: gettext\/xml\] \1/" >> po/POTFILES.in
 cat po/POTFILES.in | sort > po/POTFILES.in
