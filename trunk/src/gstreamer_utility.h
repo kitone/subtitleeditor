7744bfe0cbd612a4808c55ee7751c308c83abc31
kitone <kitone@bc142938-0c22-0410-9269-ec09724cb74d>
2009-12-30 15:19:29 +0000

    Add GSTREAMERMM_CHECK_VERSION. Support gstreamermm version >= 0.10.3 without patch.

--- trunk/src/gstreamer_utility.h
+++ trunk/src/gstreamer_utility.h
@@ -26,6 +26,12 @@
 #include <list>
 #include <glibmm/ustring.h>
 
+#define GSTREAMERMM_CHECK_VERSION(major,minor,micro) \
+	(GSTREAMERMM_MAJOR_VERSION > (major) || \
+	(GSTREAMERMM_MAJOR_VERSION == (major) && GSTREAMERMM_MINOR_VERSION > (minor)) || \
+	(GSTREAMERMM_MAJOR_VERSION == (major) && GSTREAMERMM_MINOR_VERSION == (minor) && \
+	GSTREAMERMM_MICRO_VERSION >= (micro)))
+
 namespace gstreamer_utility {
 
 	/*
