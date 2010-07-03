35b3d2a58a0a7d0ba361822d8fd63570c97b402b
kitone <kitone@bc142938-0c22-0410-9269-ec09724cb74d>
2010-07-03 07:14:27 +0000

    Fix sort command.

--- trunk/prepare-po.sh
+++ trunk/prepare-po.sh
@@ -7,4 +7,4 @@ find plugins -type f | grep '\.\(h\|cc\|ui\)$' | sed -e "s/\.\///g" >> po/POTFIL
 find plugins -type f | grep '\.se-plugin\.in$' | sed -e "s/\(.*\)/\[type: gettext\/ini\] \1/" >> po/POTFILES.in
 find plugins -type f | grep '\.se-pattern\.in$' | sed -e "s/\(.*\)/\[type: gettext\/xml\] \1/" >> po/POTFILES.in
 find plugins -type f | grep '\.ui$' | sed -e "s/\(.*\)/\[type: gettext\/glade\] \1/" >> po/POTFILES.in
-cat po/POTFILES.in | sort > po/POTFILES.in
+sort po/POTFILES.in -o po/POTFILES.in
