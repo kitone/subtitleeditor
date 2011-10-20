c963381f33db14a8b07185319f9dbead1d620047
kitone <kitone@users.noreply.github.com>
2011-10-20 17:40:15 +0000

    Failed to compile.

--- plugins/actions/documentsnavigation/documentsnavigation.cc
+++ plugins/actions/documentsnavigation/documentsnavigation.cc
@@ -54,22 +54,22 @@ public:
 
 		action_group->add(
 				Gtk::Action::create("documentsnavigation-first-document", Gtk::Stock::GOTO_FIRST, _("_First Document")),
-					sigc::bind(
+					sigc::bind<int>(
 						sigc::mem_fun(*this, &DocumentsNavigationPlugin::on_select_document), FIRST));
 
 		action_group->add(
 				Gtk::Action::create("documentsnavigation-last-document", Gtk::Stock::GOTO_LAST,  _("_Last Document")),
-					sigc::bind(
+					sigc::bind<int>(
 						sigc::mem_fun(*this, &DocumentsNavigationPlugin::on_select_document), LAST));
 
 		action_group->add(
 				Gtk::Action::create("documentsnavigation-previous-document", Gtk::Stock::GO_BACK, _("_Previous Document")),
-					sigc::bind(
+					sigc::bind<int>(
 						sigc::mem_fun(*this, &DocumentsNavigationPlugin::on_select_document), PREVIOUS));
 
 		action_group->add(
 				Gtk::Action::create("documentsnavigation-next-document", Gtk::Stock::GO_FORWARD, _("_Next Document")),
-					sigc::bind(
+					sigc::bind<int>(
 						sigc::mem_fun(*this, &DocumentsNavigationPlugin::on_select_document), NEXT));
 
 		// ui
