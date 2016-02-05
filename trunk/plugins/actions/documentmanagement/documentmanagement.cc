2d54fa9b2240ce5b526c0feba7ff17eec103b6e9
kitone <kitone@bc142938-0c22-0410-9269-ec09724cb74d>
2016-02-05 18:16:44 +0000

    Fix bug #23714 : include memory and build string using compose.

--- trunk/plugins/actions/documentmanagement/documentmanagement.cc
+++ trunk/plugins/actions/documentmanagement/documentmanagement.cc
@@ -178,9 +178,9 @@ public:
 
 		ui_id = ui->new_merge_id();
 
-		#define ADD_UI(name) ui->add_ui(ui_id, "/menubar/menu-file/"name, name, name);
-		#define ADD_OPEN_UI(name) ui->add_ui(ui_id, "/menubar/menu-file/menu-open/"name, name, name);
-		#define ADD_SAVE_UI(name) ui->add_ui(ui_id, "/menubar/menu-file/menu-save/"name, name, name);
+		#define ADD_UI(name) ui->add_ui(ui_id, Glib::ustring::compose("%1/%2","/menubar/menu-file", name), name, name);
+		#define ADD_OPEN_UI(name) ui->add_ui(ui_id, Glib::ustring::compose("%1/%2","/menubar/menu-file/menu-open", name), name, name);
+		#define ADD_SAVE_UI(name) ui->add_ui(ui_id, Glib::ustring::compose("%1/%2","/menubar/menu-file/menu-save", name), name, name);
 
 		ADD_UI("new-document");
 		ADD_OPEN_UI("open-document");
